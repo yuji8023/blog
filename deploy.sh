2f41ac36452af9656568c026824a47a9233b64cd
yuji_hu@163.com <yuji_hu@163.com>
2021-03-11 15:06:10 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 # 确保脚本抛出遇到的错误
 set -e
 
@@ -16,9 +18,8 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git remote add origin https://yuji8023:${SSH}@gitee.com/yuji8023/blog.git
+
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@gitee.com:yuji8023/blog.git master:gh-pages
+git push -f git@github.com:yuji8023/blog.git master:gh-pages
 
 cd -
